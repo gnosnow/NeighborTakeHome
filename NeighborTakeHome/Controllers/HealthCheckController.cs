3a9c986373db8ab0795e74f56023dfb2435baa7b
Gavin Monson <gnosnow@msn.com>
2025-05-20 17:59:12 -0600

    Add project files.

---
+++ NeighborTakeHome/Controllers/HealthCheckController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok(new { status = "Healthy", timestamp = DateTime.UtcNow });
+}
